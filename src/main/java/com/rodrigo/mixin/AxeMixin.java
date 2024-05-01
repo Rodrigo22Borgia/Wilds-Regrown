a6cf1c6bae802bfd5b9243b2b987588ca12bacff
Rodrigo <vojta.vesecky@seznam.cz>
2024-05-01 03:24:14 +0200

    Upload

--- src/main/java/com/rodrigo/mixin/AxeMixin.java
+++ src/main/java/com/rodrigo/mixin/AxeMixin.java
@@ -1,5 +1,6 @@
 package com.rodrigo.mixin;
 
+import com.rodrigo.WildsRegrown;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PillarBlock;
 import net.minecraft.item.AxeItem;
@@ -14,9 +15,9 @@ import static com.rodrigo.block.WRBlocks.STRIPPED;
 
 @Mixin(AxeItem.class)
 public class AxeMixin {
-	@Inject(at = @At("TAIL"), method = "getStrippedState", cancellable = true)
+	@Inject(at = @At("RETURN"), method = "getStrippedState", cancellable = true)
 	private void getStrippedState(BlockState state, CallbackInfoReturnable<Optional<BlockState>> cir) {
-		if (cir.getReturnValue().isEmpty()) {
+		if (cir.getReturnValue().isEmpty() && state.getBlock().getTranslationKey().startsWith("block." + WildsRegrown.MOD_ID)) {
 			cir.setReturnValue(Optional.ofNullable(STRIPPED.get(state.getBlock())).map((block) -> block.getDefaultState().with(PillarBlock.AXIS, state.get(PillarBlock.AXIS)))
 		);}
 	}
